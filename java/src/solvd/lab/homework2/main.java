8e2ee206a8b6a9f3fbb6787d97d2f51bbb905af7
n0szczu <patryk.noszczynski@gmail.com>
2023-10-28 03:43:46 +0200

    first homework

--- java/src/solvd/lab/homework2/main.java
+++ java/src/solvd/lab/homework2/main.java
@@ -1,10 +1,11 @@
-package solvd.lab.homework2;
+package solvd.lab.Homework2;
 
-
-public class main {
+public class Main {
     public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
 
-        System.out.print("Test commit");
-
+            System.out.println("i = " + i);
+        }
     }
 }
